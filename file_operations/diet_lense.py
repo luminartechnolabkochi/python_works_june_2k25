aa8640864a35ad349b5bd2c543e17046c4b5e3a3
sajay <sajaykannan10@gmail.com>
2025-08-29 08:38:57 +0530

    job data set added

--- file_operations/diet_lense.py
+++ file_operations/diet_lense.py
@@ -2,18 +2,14 @@
 
 file_path = "C:\\Users\\Luminar\\Desktop\\development_journey_june\\python_works\\file_operations\\food_logs.csv"
 
-
 fr = open(file_path,"r")
-
 
 food_logs = []
 
 for line in fr:#2025-08-22,Breakfast,Poha,1 plate,280\n
 
 
-    data = line.rstrip("\n").split(",")
-
-  
+    data = line.rstrip("\n").split(",")  
 
     if len(data)>1:
 
@@ -30,5 +26,26 @@ for line in fr:#2025-08-22,Breakfast,Poha,1 plate,280\n
 
 print(food_logs)
 
+daily_summary ={}
+
+for e in food_logs:
+
+    date = e.get("date")
+
+    calories = int(e.get("calories"))
+
+    if date in daily_summary:
+
+        daily_summary[date]+=calories
+
+    else:
+
+        daily_summary[date] = calories
+
+print(daily_summary)
 
 
+# Topic for tomorrow : Decision making (if..else,if elif,else)
+
+
+# Task .csv
